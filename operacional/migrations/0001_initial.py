0eae61c177e4749797f8665e5fa269c60b312315
fabionm <fabio.n.m.h@gmail.com>
2016-04-19 18:11:29 -0700

    Layout Checkin operacional e da lista de checkins

--- operacional/migrations/0001_initial.py
+++ operacional/migrations/0001_initial.py
@@ -20,7 +20,31 @@ class Migration(migrations.Migration):
                 ('dia_agendamento', models.DateField()),
                 ('hora_agendamento', models.TimeField()),
                 ('observacao', models.TextField(blank=True)),
+                ('motivo', models.CharField(default=b'creditarestoque', max_length=20, choices=[(b'creditarestoque', b'CreditarEstoque'), (b'comprafornecedor', b'CompraFornecedor')])),
+                ('canal', models.ManyToManyField(to='administrativo.Canal')),
+                ('loja', models.ForeignKey(to='administrativo.Loja')),
                 ('marca', models.ForeignKey(blank=True, to='administrativo.Marca', null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Checkout',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('motivo', models.CharField(max_length=20, choices=[(b'emprestimo', b'Emprestimo'), (b'consignacao', b'Consignacao'), (b'avaria', b'Avaria'), (b'venda', b'Venda')])),
+                ('dia', models.DateField(auto_now_add=True)),
+                ('hora', models.TimeField(auto_now_add=True)),
+                ('observacao', models.TextField(blank=True)),
+                ('loja', models.ForeignKey(to='administrativo.Loja')),
+                ('marca', models.ForeignKey(to='administrativo.Marca')),
+                ('periodo', models.ForeignKey(blank=True, to='administrativo.Periodo', null=True)),
+            ],
+        ),
+        migrations.CreateModel(
+            name='Estoque_Loja',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('quantidade', models.IntegerField()),
+                ('loja', models.ForeignKey(to='administrativo.Loja')),
             ],
         ),
         migrations.CreateModel(
@@ -42,15 +66,27 @@ class Migration(migrations.Migration):
                 ('largura', models.IntegerField(verbose_name=b'largura')),
                 ('altura', models.IntegerField(verbose_name=b'altura')),
                 ('profundidade', models.IntegerField(verbose_name=b'profundidade')),
-                ('quantidade', models.IntegerField(null=True, verbose_name=b'quantidade', blank=True)),
+                ('quantidade', models.IntegerField(default=0, verbose_name=b'quantidade')),
                 ('preco_base', models.FloatField(null=True, verbose_name=b'pre\xc3\xa7o base', blank=True)),
                 ('preco_venda', models.FloatField(null=True, verbose_name=b'pre\xc3\xa7o venda', blank=True)),
                 ('em_estoque', models.CharField(default=b'nao', max_length=5, choices=[(b'sim', b'Sim'), (b'nao', b'Nao')])),
+                ('espaco', models.ForeignKey(blank=True, to='administrativo.Espaco', null=True)),
+                ('loja', models.ManyToManyField(to='administrativo.Loja', through='operacional.Estoque_Loja')),
                 ('marca', models.ForeignKey(to='administrativo.Marca')),
             ],
         ),
         migrations.AddField(
             model_name='expedicao',
+            name='produto',
+            field=models.ForeignKey(to='operacional.Produto'),
+        ),
+        migrations.AddField(
+            model_name='estoque_loja',
+            name='produto',
+            field=models.ForeignKey(to='operacional.Produto'),
+        ),
+        migrations.AddField(
+            model_name='checkout',
             name='produto',
             field=models.ForeignKey(to='operacional.Produto'),
         ),
@@ -62,5 +98,9 @@ class Migration(migrations.Migration):
         migrations.AlterUniqueTogether(
             name='expedicao',
             unique_together=set([('checkin', 'produto')]),
+        ),
+        migrations.AlterUniqueTogether(
+            name='estoque_loja',
+            unique_together=set([('produto', 'loja')]),
         ),
     ]
