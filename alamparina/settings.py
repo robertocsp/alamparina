4c3a87bbc6491a697449e5d01e1a80d25144b71e
Rodrigo Faillace <rodrigo_fallace@hotmail.com>
2016-05-14 00:54:52 -0300

    eb

--- alamparina/settings.py
+++ alamparina/settings.py
@@ -105,7 +105,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
-STATIC_ROOT = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'alamparina/static')
+STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
 
 STATIC_URL = '/static/'
 
