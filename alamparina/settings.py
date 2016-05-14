4135e56b4ee9a01c7e0efc986010e6efc1106f41
Rodrigo Faillace <rodrigo_fallace@hotmail.com>
2016-05-14 15:27:48 -0300

    eb - static

--- alamparina/settings.py
+++ alamparina/settings.py
@@ -10,8 +10,7 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
@@ -22,15 +21,14 @@ SECRET_KEY = '%1e*xipej7@2ir6d7d)$ra*-e67l)q-@c7-376g)(t$0tb20gz'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+ALLOWED_HOSTS = ['*']
+
+
 ADMINS = (
     ("Roberto", "80.pereira@gmail.com") #envia email caso ocorra erro 500 e debug=false
 )
 
-
 TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -103,19 +101,52 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_ROOT = os.path.join(BASE_DIR, "..", "www", "static")
+# https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
 MEDIA_URL = '/media/'
 
-TEMPLATE_DIRS = [(os.path.join(BASE_DIR, 'templates'))]
+AWS_HEADERS = {  # see http://developer.yahoo.com/performance/rules.html#expires
+        'Expires': 'Thu, 31 Dec 2099 20:00:00 GMT',
+        'Cache-Control': 'max-age=94608000',
+    }
 
-LOGIN_URL ='/login/'
+
+ALAMPARINA_S3_ACCESS_KEY_DEV = 'AKIAISC7E346KMSITNLQ'
+ALAMPARINA_S3_SECRET_ACCESS_KEY = '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'
+
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_ALAMPARINA_STORAGE', 'alamparina')
+AWS_ACCESS_KEY_ID = os.environ.get('ALAMPARINA_AWS_ACCESS_KEY', ALAMPARINA_S3_ACCESS_KEY_DEV)
+AWS_SECRET_ACCESS_KEY = os.environ.get('ALAMPARINA_AWS_SECRET_ACCESS_KEY', ALAMPARINA_S3_SECRET_ACCESS_KEY)
+
+# Tell django-storages that when coming up with the URL for an item in S3 storage, keep
+# it simple - just use this domain plus the path. (If this isn't set, things get complicated).
+# This controls how the `static` template tag from `staticfiles` gets expanded, if you're using it.
+# We also use it in the next setting.
+AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+#AWS_S3_CUSTOM_DOMAIN = 'weikedev.s3-website-sa-east-1.amazonaws.com'
+
+# This is used by the `static` template tag from `static`, if you're using that. Or if anything else
+# refers directly to STATIC_URL. So it's safest to always set it.
+STATIC_URL = "https://%s/static/" % AWS_S3_CUSTOM_DOMAIN
+MEDIA_URL = "https://%s/" % AWS_S3_CUSTOM_DOMAIN
+
+# Tell the staticfiles app to use S3Boto storage when writing the collected static files (when
+# you run `collectstatic`).
+STATICFILES_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto.S3BotoStorage'
+
+AWS_LOCATION = ''
+
+DAB_FIELD_RENDERER = 'django_admin_bootstrapped.renderers.BootstrapFieldRenderer'
+
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'alert-success success',
+    messages.WARNING: 'alert-warning warning',
+    messages.ERROR: 'alert-danger error'
+}
 
 TEMPLATES = [
     {
